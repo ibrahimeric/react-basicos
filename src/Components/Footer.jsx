de5d7767ae2bd1a9ae09089ef471b0d13bd56910
Eric Ibrahim <ericibrahim19@gmail.com>
2023-05-06 19:42:56 -0300

    commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,6 +5,10 @@ const Footer = () => {
   return (
     <div class="footer">
       Este sera el pie de pagina
+      <ul>
+        <li>lista1</li>
+        <li>lista2</li>
+      </ul>
     </div>
   )
 }
