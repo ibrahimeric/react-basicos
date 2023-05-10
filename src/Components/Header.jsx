57df376c36e31f3a6190003e6c8356eef85375b0
Thomas Valla <thomasvalla7@gmail.com>
2023-05-09 21:46:38 -0300

    Actualizacion de los nombres de las clases del Header.html y Header.css para evitar conflictos con otras hojas de estilo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,52 +5,52 @@ const Header = () => {
   return (
     <div>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
-        <nav class="nav">
-            <a class="navlogo" href="#"><img src="https://cdn.pixabay.com/photo/2017/03/16/21/18/logo-2150297_1280.png" alt="logo"/></a>
+        <nav class="Hnav">
+            <a class="Hnavlogo" href="#"><img src="https://cdn.pixabay.com/photo/2017/03/16/21/18/logo-2150297_1280.png" alt="logo"/></a>
 
-            <div class="navDiv">
-                <ul class="navList">
-                    <li class="navItem">
-                        <a class="navLink" href="#">Inicio</a>
+            <div class="HnavDiv">
+                <ul class="HnavList">
+                    <li class="HnavItem">
+                        <a class="HnavLink" href="#">Inicio</a>
                     </li>
-                    <li class="navItem">
-                        <a class="navLink" href="#">Contactanos</a>
+                    <li class="HnavItem">
+                        <a class="HnavLink" href="#">Contactanos</a>
                     </li>
-                    <li class="navItem">
-                        <a class="navLink" href="#">Categorias <i class="toggle-dropdown dropdown-toggle"></i></a>
-                        <ul className='dropdown'>
-                            <li className="dropdowsItem">
-                            <a class="dropLink" href="#">Campreras</a>
+                    <li class="HnavItem">
+                        <a class="HnavLink" href="#">Categorias <i class="Htoggle-dropdown dropdown-toggle"></i></a>
+                        <ul className='Hdropdown'>
+                            <li className="HdropdowsItem">
+                            <a class="HdropLink" href="#">Campreras</a>
                             </li>
-                            <li className="dropdowsItem">
-                                <a class="dropLink" href="#">Remeras</a>
+                            <li className="HdropdowsItem">
+                                <a class="HdropLink" href="#">Remeras</a>
                             </li>
-                            <li className="dropdowsItem">
-                            <a class="dropLink" href="#">Pantalones</a>
+                            <li className="HdropdowsItem">
+                            <a class="HdropLink" href="#">Pantalones</a>
                             </li>
-                            <li className="dropdowsItem">
-                            <a class="dropLink" href="#">Shorts</a>
+                            <li className="HdropdowsItem">
+                            <a class="HdropLink" href="#">Shorts</a>
                             </li>
                         </ul>
                     </li>
                 </ul>
             </div>
-            <div className="cart">
-                <ul className='cartList'>
-                    <li className="buy"><a className='buy-a' href="#"><i class="fa fa-shopping-cart"></i></a></li>
+            <div className="Hcart">
+                <ul className='HcartList'>
+                    <li className="Hbuy"><a className='Hbuy-a' href="#"><i class="fa fa-shopping-cart"></i></a></li>
                 </ul>
             </div>
-            <div className="icons">
-                <ul className='iconList'>
-                    <li className="icon"><a className='facebook' href="#"><i class="fa fa-facebook"></i></a></li>
-                    <li className="icon"><a className='instagram' href="#"><i class="fa fa-instagram"></i></a></li>
-                    <li className="icon"><a className='twitter' href="#"><i class="fa fa-twitter"></i></a></li>
+            <div className="Hicons">
+                <ul className='HiconList'>
+                    <li className="Hicon"><a className='Hfacebook' href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li className="Hicon"><a className='Hinstagram' href="#"><i class="fa fa-instagram"></i></a></li>
+                    <li className="Hicon"><a className='Htwitter' href="#"><i class="fa fa-twitter"></i></a></li>
                 </ul>
             </div>
-            <div className="user">
-                <ul className='userList'>
-                    <li className="login"><a href="#"><i class="fa fa-sign-in"></i></a></li>
-                    <li className="profile"><a href="#"><i class="fa fa-user"></i></a></li>
+            <div className="Huser">
+                <ul className='HuserList'>
+                    <li className="Hlogin"><a href="#"><i class="fa fa-sign-in"></i></a></li>
+                    <li className="Hprofile"><a href="#"><i class="fa fa-user"></i></a></li>
                 </ul>
             </div>
         </nav>
