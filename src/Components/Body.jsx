cd63b7ad9384b1837c16e0c317b0a42c69195411
Thomas Valla <thomasvalla7@gmail.com>
2023-05-10 22:35:34 -0300

    Añadí funcionalidad al carrito de compras

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -4,74 +4,43 @@ import air_jordan1 from '../img/air_jordan1.jpg'
 import nike_airmax97 from '../img/nike_airmax97.jpg'
 import adidas_dayjogger from '..//img/adidas_dayjogger.jpg'
 import fondo from '..//img/fondo.jpg'
+import {data} from '../Js/data.js'
 
-const Principal = () => {
+const Principal = ({allProducts, setAllProducts, countProducts, setCountProducts, total, setTotal}) => {
+  
+  const onAddProduct = product => {
+    if(allProducts.find(item => item.id === product.id)){
+      const products = allProducts.map(item => item.id === product.id? {...item, quantity: item.quantity +1} : item);
+      
+      setTotal(total + product.price * product.quantity);
+      setCountProducts(countProducts + product.quantity);
+      return setAllProducts([...products]);
+    }
+
+    setTotal(total + product.price * product.quantity);
+    setCountProducts(countProducts + product.quantity);
+    setAllProducts([...allProducts, product]);
+  };
+
   return (
-    <div class="body" >
+    <div className="body" >
     
         <h1>PRODUCTOS DE OFERTA</h1>
         
-       <div class="fondo"></div>
-        <div class="container">
-            <div class="card">
-           <center>   <img src={air_jordan1} /> </center>
-           <p>AIR JORDAN 1
-              $1200
-           </p>
-            </div>
-
-            <div class="card">
-             <center> <img src={nike_airmax97} /></center>
-            </div>
-
-            <div class="card">
-          <center><img src={adidas_dayjogger} /></center>
-            </div>
-
-            <div class="card">
-           <center>   <img src={air_jordan1} /> </center>
-            </div>
-
-            <div class="card">
-             <center> <img src={nike_airmax97} /></center>
-            </div>
-
-            <div class="card">
-          <center><img src={adidas_dayjogger} /></center>
-            </div>
-            <div class="card">
-           <center>   <img src={air_jordan1} /> </center>
-            </div>
-
-            <div class="card">
-             <center> <img src={nike_airmax97} /></center>
-            </div>
-            <div class="card">
-           <center>   <img src={air_jordan1} /> </center>
-            </div>
-            <div class="card">
-           <center>   <img src={air_jordan1} /> </center>
-            </div>
-            <div class="card">
-           <center>   <img src={air_jordan1} /> </center>
-            </div>
-            <div class="card">
-           <center>   <img src={air_jordan1} /> </center>
-            </div>
-            <div class="card">
-           <center>   <img src={air_jordan1} /> </center>
-            </div>
-            <div class="card">
-           <center>   <img src={air_jordan1} /> </center>
-            </div>
-            <div class="card">
-           <center>   <img src={air_jordan1} /> </center>
-            </div>
-
+       <div className="fondo"></div>
+        <div className="container">
+              {data.map(product => (
+                <div className="card" key={product.id}>
+                  <center>   <img src={product.urlImage} alt={product.nameProduct} /> </center>
+                  <h6>{product.nameProduct}</h6>
+                  <p>${product.price}</p>
+                  <button onClick={() => onAddProduct(product)}>Añadir al carrito</button>
+                </div>
+              ))}
         </div> 
-        <div class="imgn"></div>
+        <div className="imgn"></div>
     </div>
-  )
-}
+  );
+};
 
 export default Principal
---
+++ src/Js/data.js
@@ -0,0 +1,32 @@
+import air_jordan1 from '../img/air_jordan1.jpg';
+
+export const data = [
+    {
+        id:1,
+        nameProduct: 'AIR JORDAN 1',
+        price: 1200,
+        urlImage: air_jordan1,
+        quantity: 1,
+    },
+    {
+        id:2,
+        nameProduct: 'AIR JORDAN 2',
+        price: 1201,
+        urlImage: air_jordan1,
+        quantity: 1,
+    },
+    {
+        id:3,
+        nameProduct: 'AIR JORDAN 3',
+        price: 1202,
+        urlImage: air_jordan1,
+        quantity: 1,
+    },
+    {
+        id:4,
+        nameProduct: 'AIR JORDAN 4',
+        price: 1203,
+        urlImage: air_jordan1,
+        quantity: 1,
+    },
+];
