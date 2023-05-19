21fdcea8a3ce0ceaed474c5d709cf7b88cacdded
Franco <francolox13@gmail.com>
2023-05-18 23:13:22 -0300

    slider

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,23 +1,47 @@
-import React from 'react'
+import Carousel from 'react-bootstrap/Carousel';
 
-const Slider = () => {
+function Slider() {
   return (
-    <div>
-        Este es un slider
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 h-100"
+          src="https://images.hdqwalls.com/download/lai-da-palpuogna-lake-in-switzerland-5k-9v-3840x2160.jpg"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>hola</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 h-100"
+          src="https://images.hdqwalls.com/download/seagulls-in-switzerland-3840x2160.jpg"
+          alt="Second slide"
+        />
 
-        <div className="div">
-            <ul>
-                <li>lista1</li>
-                <li>lista2</li>
-            </ul>
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>alfin</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 h-100"
+          src="https://images.hdqwalls.com/download/forests-czech-republic-parks-bohemian-switzerland-am-3840x2160.jpg"
+          alt="Third slide"
+        />
 
-            <ul>
-                <li>nombre</li>
-                <li>apellido</li>
-            </ul>
-        </div>
-    </div>
-  )
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            xd
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
 }
 
-export default Slider
+export default Slider;
